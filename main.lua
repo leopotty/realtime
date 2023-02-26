593e4ba856f0f8a6c024254d8aa42910fe1e188e
x <91684842+leopotty@users.noreply.github.com>
2023-02-26 16:17:16 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
 local dateTime = DateTime.now()
 local universalTime = dateTime:ToUniversalTime()
-local GMTTime = universalTime.Hour + 1 + universalTime.Minute + 1 + universalTime.Millisecond + 1
-game.Lighting.ClockTime = GMTTime
+local GMTHour = universalTime.Hour + 1
+local GMTMin = universalTime.Minute + 1
+local GMTMil = universalTime.Millisecond + 1
+game.Lighting.ClockTime = GMTHour
+game.Lighting.ClockTime = GMTMin
+game.Lighting.ClockTime = GMTMil
